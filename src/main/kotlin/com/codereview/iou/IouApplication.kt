6a73e9e3299c80621060878abfe639fefa36cc42
SFomin <sergei.fomin@list.ru>
2021-11-09 14:42:53 +0500

    first commit

---
+++ src/main/kotlin/com/codereview/iou/IouApplication.kt
@@ -0,0 +1,11 @@
+package com.codereview.iou
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class IouApplication
+
+fun main(args: Array<String>) {
+	runApplication<IouApplication>(*args)
+}
---
+++ src/test/kotlin/com/codereview/iou/IouApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.codereview.iou
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class IouApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
